f1ad1cd1927ae61b95bc363b3b013cb98ed562ed
Sander6655 <131519999+Sander6655@users.noreply.github.com>
2023-11-03 10:35:23 +0100

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ print("Turn your wifi off!")
 ssid = input("What is the mame of your new wifi network?\n Name: ")
 password = input("What is the new password of your wifi network?\n password: ")
 
-command = 'sudo ./create_ap wlp2s0 eno1 ' + ssid + ' ' + password
+command = 'sudo ./create_ap wlan0 eth0 ' + ssid + ' ' + password
 
 subprocess.call(command, shell=True)
 
